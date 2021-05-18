48b64bde9da02c107653ee421c23c18a54985d4a
Wyschy <77229878+Wyschy@users.noreply.github.com>
2021-05-18 19:26:19 +0200

    Add files via upload

--- M5Core2_colortouch.h
+++ M5Core2_colortouch.h
@@ -12,7 +12,7 @@
       char buttontext;
 
       public:
-      void setup(int x, int y, int w, int h, int frame, int area, int pressed) {
+      void setup(int x, int y, int w, int h, int frame, int area, int pressed, char text) {
         buttonx = x;
         buttony = y;
         buttonw = w;
@@ -20,10 +20,13 @@
         framecolor = frame;
         buttoncolor = area;
         buttoncolorpressed = pressed;
+        buttontext = text;
        
         
         M5.Lcd.drawRoundRect(buttonx,buttony,buttonw,buttonh,10,framecolor);
         M5.Lcd.fillRoundRect(buttonx+1,buttony+1,buttonw-2,buttonh-2,10,buttoncolor);
+        M5.Lcd.setCursor(buttonx+10,buttony+10);
+        M5.Lcd.print(buttontext);
         
       }
       bool bpress()  {
@@ -32,6 +35,8 @@
         Event& e = M5.Buttons.event;
         if (e & (E_MOVE | E_RELEASE)) {
           M5.Lcd.fillRoundRect(buttonx+1,buttony+1,buttonw-2,buttonh-2,10,buttoncolor);
+          M5.Lcd.setCursor(buttonx+10,buttony+10);
+          M5.Lcd.print(buttontext);
           buttonstate = false;
         }
         
@@ -39,6 +44,58 @@
           Point p = e & E_MOVE ? e.from : e.to;
           if (p.x > buttonx && p.x < buttonx+buttonw && p.y > buttony && p.y < buttony+buttonh) {
             M5.Lcd.fillRoundRect(buttonx+1,buttony+1,buttonw-2,buttonh-2,10,buttoncolorpressed);
+            M5.Lcd.setCursor(buttonx+10,buttony+10);
+            M5.Lcd.print(buttontext);
+            buttonstate = true;
+          }
+        }
+      
+      return (buttonstate);
+      }
+        
+    };
+
+class Frametouch  {
+      private:
+      int framecolor;
+      int buttonx;
+      int buttony;
+      int buttonw;
+      int buttonh;
+      bool buttonstate;
+      char buttontext;
+
+      public:
+      void setup(int x, int y, int w, int h, int frame, char text) {
+        buttonx = x;
+        buttony = y;
+        buttonw = w;
+        buttonh = h;
+        framecolor = frame;
+        buttontext = text;       
+        
+        M5.Lcd.drawRoundRect(buttonx,buttony,buttonw,buttonh,10,framecolor);
+        M5.Lcd.setCursor(buttonx+10,buttony+10);
+        M5.Lcd.print(buttontext);
+        
+      }
+      bool bpress()  {
+        //M5.update();
+
+        Event& e = M5.Buttons.event;
+        if (e & (E_MOVE | E_RELEASE)) {
+          M5.Lcd.drawRoundRect(buttonx,buttony,buttonw,buttonh,10,framecolor);
+          M5.Lcd.setCursor(buttonx+10,buttony+10);
+          M5.Lcd.print(buttontext);
+          buttonstate = false;
+        }
+        
+        if (e & (E_TOUCH | E_MOVE)) {
+          Point p = e & E_MOVE ? e.from : e.to;
+          if (p.x > buttonx && p.x < buttonx+buttonw && p.y > buttony && p.y < buttony+buttonh) {
+            M5.Lcd.drawRoundRect(buttonx,buttony,buttonw,buttonh,10,BLACK);
+            M5.Lcd.setCursor(buttonx+10,buttony+10);
+            M5.Lcd.print(buttontext);
             buttonstate = true;
           }
         }
